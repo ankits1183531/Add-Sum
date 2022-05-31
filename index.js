47ede2027194ddac3a84e9665c9882569e5b0f2a
ankits1183531 <ankits1183531@gmail.com>
2022-05-31 14:05:13 +0530

    Change delimeter solution

--- index.js
+++ index.js
@@ -9,8 +9,20 @@ function addSum(a) {
   if (checkIfEmpty(a)) {
     return 0;
   } else {
-    return getSum(getNumbers(a));
+    const delimeter = delimeterChangePatternCheck(a);
+    if (delimeter !== ',') {
+      a = a.substring(4);
+    }
+    return getSum(getNumbers(a, delimeter));
   }
+}
+
+function delimeterChangePatternCheck(input) {
+  let delimeter = ',';
+  if (input.substring(0, 2) === '//' && input.substring(3, 4) === '\n') {
+    delimeter = input.substring(2, 3);
+  }
+  return delimeter;
 }
 
 function getSum(arr) {
@@ -19,16 +31,17 @@ function getSum(arr) {
     if (Number(num) > 0) {
       sum += Number(num);
     } else {
-      throw new Error('Negative Number Found');
+      throw new Error('negatives not allowed');
     }
   }
   return sum;
 }
 
-function getNumbers(input) {
+function getNumbers(input, delimeter) {
+  console.log();
   const arr = input
-    .replaceAll('\n', ',')
-    .split(',')
+    .replaceAll('\n', delimeter)
+    .split(delimeter)
     .filter((el) => el != '');
   return arr;
 }
@@ -47,4 +60,7 @@ console.log(addSum('1 ,2,3,4,5'));
 console.log(addSum('1\n2,3'));
 console.log(addSum('1\n2,3\n'));
 console.log(addSum('1,\n'));
+
+console.log(addSum('//;\n1;2'));
+
 console.log(addSum('1,2,-3'));
